6e41cee6920c26b809ea37ee301167491ce7d882
Joe Brew <joebrew@gmail.com>
2014-10-14 15:37:54 -0400

    contour map

--- public/mosquito/SurfaceFun.R
+++ public/mosquito/SurfaceFun.R
@@ -1,9 +1,10 @@
 library(gstat)
 library(geoR)
+load("C:/Users/BrewJR/Documents/fdoh/public/mosquito/reports/2014-10-14/master.RData")
 
 disease <- "wnv"
 
-SurfaceFun <- function(disease){
+SurfaceFun <- function(disease = "wnv"){
   temp <- master[,which(grepl(disease ,colnames(master)) & 
                           grepl("site", colnames(master)))]
   temp <- as.data.frame(cbind(date=master$date, temp))
@@ -14,53 +15,86 @@ SurfaceFun <- function(disease){
   a <- data.frame("x" = loc$lon,
                   "y" = loc$lat,
                   "z" = temp)
+  
   b <- as.geodata(a)
   
-  # Predict one point
-  prediction <- ksline(b, cov.model="exp", cov.pars=c(10,3.33), nugget=0,
-                       locations=c(-82.15,29.323))
-  
   # Predict multiple points
-   x <- seq(-83, -82, by=0.01)
-   y <- seq(29,30, by=0.01)
-   xv <- rep(x,14)
-   yv <- rep(y, each=14)
-   in_mat <- as.matrix(cbind(xv,yv))
-
+  x <- seq(-83, -82, by=0.01)
+  y <- seq(29,30, by=0.01)
+  xv <- rep(x,each=14)
+  yv <- rep(y, 14)
+  in_mat <- as.matrix(cbind(xv,yv))
+  #   in_mat <- data.frame(in_mat)
+  #   names(in_mat) <- c("x", "y")
+  #   in_mat <- as.geodata(in_mat)
   # 
-  q <- ksline(b, cov.model="exp",cov.pars=c(10,3.33), nugget=0,
-              locations=in_mat)
-  q$predict
-  image(q, val=q$predict)
-  image(q)
   
+  # Predict using kriging (another method)
+  mykrig <- krige.conv(geodata = b, coords = b$coords, data = b$data,
+                       locations = in_mat,
+                       # borders = ALACHUA BORDERS!,
+                       krige = krige.control(type.krige = "ok",
+                                             cov.pars = c(10, 3.33)))
+  
+  library(scatterplot3d)
+  scatterplot3d(in_mat[,1], in_mat[,2], mykrig$predict)
+  
+  # defining the grid
+  pred.grid <-  expand.grid(seq(-83,-82, l=100), seq(29,30, l=100))
+  # kriging calculations
+  kc <- krige.conv(geodata = b, coords = b$coords, data = b$data,
+                   locations = pred.grid,
+                   # borders = ALACHUA BORDERS!,
+                   krige = krige.control(type.krige = "ok",
+                                         cov.pars = c(10, 3.33)))
+  # displaying predicted values
+  image(kc, loc = pred.grid, 
+        #col = c("darkblue", "blue", "lightblue", "white", "pink", "red", "darkred"),
+        col=gray(seq(1,0.1,l=100)), 
+        xlab="Coord X", ylab="Coord Y")
+  
+  library(maps)
+  map("county", "fl", add = T)
+  
+  # http://www.leg.ufpr.br/geoR/geoRdoc/geoRintro.html
+  par(mfrow=c(2,2))
+  image(kc, loc = pred.grid, main = "predicted", col=gray(seq(1,0.1,l=30)))
+  image(kc, val ="variance", loc = pred.grid, 
+        main = "prediction variance", col=gray(seq(1,0.1,l=30)))
+  image(kc, val = "simulation", number.col = 1, loc = pred.grid, 
+        main = "a simulation from\nthe predictive distribution", col=gray(seq(1,0.1,l=30)))
+  image(kc, val = "simulation", number.col = 2,loc = pred.grid, 
+        main = "another simulation from \n the predictive distribution", col=gray(seq(1,0.1,l=30)))
+  par(par.ori)
+  
+  #
   
   # Construct matrix in alternative way
-#    x.range <- as.integer(range(a[,1]))
-#    y.range <- as.integer(range(a[,2]))
-#    x=seq(from=x.range[1], to=x.range[2], by=1)
-#    y=seq(from=y.range[1], to=y.range[2], by=1)
-#    length(x)
-#    length(y)
-#    xv <- rep(x,length(y))
-#    yv <- rep(y, each=length(x))
-#    in_mat <- as.matrix(cbind(xv,yv))
-#    plot(in_mat)
+  #    x.range <- as.integer(range(a[,1]))
+  #    y.range <- as.integer(range(a[,2]))
+  #    x=seq(from=x.range[1], to=x.range[2], by=1)
+  #    y=seq(from=y.range[1], to=y.range[2], by=1)
+  #    length(x)
+  #    length(y)
+  #    xv <- rep(x,length(y))
+  #    yv <- rep(y, each=length(x))
+  #    in_mat <- as.matrix(cbind(xv,yv))
+  #    plot(in_mat)
   
   
   
-
+  
   
   
   
   
   mymap <- map("county", "florida", 
-      fill=TRUE, 
-      col=c(alachuacol2,countycols2),
-      xlim=c(-82.75,-82), 
-      ylim=c(29.4,30),
-      border=NA, 
-      add=F)
+               fill=TRUE, 
+               col=c(alachuacol2,countycols2),
+               xlim=c(-82.75,-82), 
+               ylim=c(29.4,30),
+               border=NA, 
+               add=F)
   title(main=toupper(disease), cex.main=0.8)
   
   #ADD TRAP POINTS
